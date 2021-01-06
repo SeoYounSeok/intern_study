bb6ae52e0dfb173d9ed72db3d1a47e0ab153e4f5
SeoYounSeok <94.younseok@gmail.com>
2021-01-06 09:04:06 +0900

    pathvariable thymeleaf 간단 사용

--- blog/site/src/main/java/net/joins/site/controller/IndexController.java
+++ blog/site/src/main/java/net/joins/site/controller/IndexController.java
@@ -31,28 +31,39 @@ public class IndexController {
         model.addAttribute("subtitle","Spring Boot + JPA + Hibernate");
 
         val item = new HashMap<String, String>();
+        item.put("id", "1");
         item.put("title", "joins.net blog");
         item.put("subtitle", "Simple blog By springboot multi module");
         item.put("href", "post");
         item.put("updated", "2020/01/04");
 
+
+        val item2 = new HashMap<String, String>();
+        item2.put("id", "2");
+        item2.put("title", "joins.net blog2");
+        item2.put("subtitle", "Simple blog By springboot multi module22");
+        item2.put("href", "post");
+        item2.put("updated", "2020/01/05");
+
         val itemList = new ArrayList<>();
         itemList.add(item);
-        itemList.add(item);
-        itemList.add(item);
+        itemList.add(item2);
+
+
         System.out.println(itemList);
         model.addAttribute("itemList",itemList);
         return "index";
 
         }
         // @PathVariable("post") String post
-        @GetMapping("/post")
-        public String post(Model model) {
+        @GetMapping("/post/{id}")
+        public String post(@PathVariable("id") String id, Model model) {
             // 이거는 하드 코딩
+            int ids = Integer.parseInt(id);
             val item = new HashMap<String, String>();
-            item.put("title", "joins.net blog");
-            item.put("subtitle", "Simple blog By springboot multi module");
-            item.put("updated", "2021/01.05");
+            item.put("title", "YounSeok's blog");
+            item.put("subtitle", "Spring Boot + JPA + Hibernate");
+            item.put("updated", "2021.01.05");
             String contents = "<h1>POST HTML TEST</h1>";
             contents += "<img class=\"img-fluid\" src=\"img/post-sample-image.jpg\" alt=\"\">";
             item.put("content", contents);
@@ -60,22 +71,25 @@ public class IndexController {
 
             model.addAttribute("item", item);
 
-            Post post = new Post();
-            post.setTitle("joins.net blog");
-            post.setSubtitle("Simple blog By springboot multi module");
-            post.setHref("post");
-            post.setBanner("images/post-bg.jpg");
-
-            String content = "<p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>";
-            content += "<h2 class=\"section-heading\">The Final Frontier</h2>";
-            content += "<img class=\"img-fluid\" src=\"img/post-sample-image.jpg\">";
-            post.setContent(content);
-
-            blogService.addPost(post);
-            blogService.addPost(post);
-            // Post List 들어온다.
+//            Post post = new Post();
+//            post.setTitle("joins.net blog");
+//            post.setSubtitle("Simple blog By springboot multi module");
+//            post.setHref("post");
+//            post.setBanner("images/post-bg.jpg");
+//
+//            String content = "<p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>";
+//            content += "<h2 class=\"section-heading\">The Final Frontier</h2>";
+//            content += "<img class=\"img-fluid\" src=\"img/post-sample-image.jpg\">";
+//            post.setContent(content);
+//
+//            blogService.addPost(post);
+//            // Post List 들어온다.
             List<Post> list = blogService.getAll();
+            System.out.println("TEST TEST TEST TEST");
+
+            // list 가 안 나와서 확인해본 결과 휘발성이었다.
             System.out.println(list);
+            System.out.println("TEST TEST TEST TEST");
             return "post";
         }
 
